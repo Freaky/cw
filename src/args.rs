fc34368b2ede5d0408f4f47122417c9c54b016a8
Thomas Hurst <tom@hur.st>
2019-07-02 17:30:53 +0100

    Simplify arg derives. Fix files-from parsing.

--- src/args.rs
+++ src/args.rs
@@ -8,28 +8,28 @@ use structopt::StructOpt;
 )]
 pub struct Opt {
     /// Count lines
-    #[structopt(short = "l", long = "lines")]
+    #[structopt(short, long)]
     pub lines: bool,
     /// Count words
-    #[structopt(short = "w", long = "words")]
+    #[structopt(short, long)]
     pub words: bool,
     /// Count bytes
-    #[structopt(short = "c", long = "bytes", overrides_with = "chars", multiple = true)]
+    #[structopt(short = "c", long, overrides_with = "chars", multiple = true)]
     pub bytes: bool,
     /// Count bytes (default) or characters (-m) of the longest line
     #[structopt(short = "L", long = "max-line-length")]
     pub longest_line: bool,
     /// Count UTF-8 characters instead of bytes
-    #[structopt(short = "m", long = "chars", overrides_with = "bytes", multiple = true)]
+    #[structopt(short = "m", long, overrides_with = "bytes", multiple = true)]
     pub chars: bool,
     /// Number of counting threads to spawn
-    #[structopt(long = "threads", default_value = "1")]
+    #[structopt(long, default_value = "1")]
     pub threads: usize,
     /// Read input from the newline-terminated list of filenames in the given file.
-    #[structopt(long = "files-from")]
+    #[structopt(long = "files-from", parse(from_os_str))]
     pub files_from: Option<PathBuf>,
     /// Read input from the NUL-terminated list of filenames in the given file.
-    #[structopt(long = "files0-from")]
+    #[structopt(long = "files0-from", parse(from_os_str))]
     pub files0_from: Option<PathBuf>,
     /// Input files
     #[structopt(parse(from_os_str))]
